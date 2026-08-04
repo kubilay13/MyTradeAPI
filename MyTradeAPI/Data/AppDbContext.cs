3aebbe69624dc6761f679c3310deb746578f4632
Kubilay <145432835+kubilay13@users.noreply.github.com>
2026-08-04 22:44:05 +0300

    Db bağlantı ayarları yapıldı.

---
+++ MyTradeAPI/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyTradeAPI.Models.UserModel;
+
+namespace MyTradeAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(
+                new User { Id = 1, Username = "kubilay", Password = "1234" }
+            );
+        }
+    }
+}
---
+++ MyTradeAPI/Models/UserModel/User.cs
@@ -0,0 +1,9 @@
+﻿namespace MyTradeAPI.Models.UserModel
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
